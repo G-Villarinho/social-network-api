4d3cc1d0a5c4f5ffe5139761a31fe67a6210d647
gabriel <gabrielvillarinho01@gmail.com>
2024-11-02 19:37:31 -0300

    feat: Implement rate limiting for like and unlike post routes in Post API

--- cmd/api/router/post.go
+++ cmd/api/router/post.go
@@ -2,17 +2,38 @@ package router
 
 import (
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/G-Villarinho/social-network/domain"
 	"github.com/G-Villarinho/social-network/middleware"
 	"github.com/G-Villarinho/social-network/pkg"
 	"github.com/labstack/echo/v4"
+	echomiddleware "github.com/labstack/echo/v4/middleware"
+	"golang.org/x/time/rate"
 )
 
 func setupPostRoutes(e *echo.Echo, di *pkg.Di) {
 	postHandler, err := pkg.Invoke[domain.PostHandler](di)
 	if err != nil {
 		log.Fatal("error to create user handler: ", err)
+	}
+
+	config := echomiddleware.RateLimiterConfig{
+		Skipper: echomiddleware.DefaultSkipper,
+		Store: echomiddleware.NewRateLimiterMemoryStoreWithConfig(
+			echomiddleware.RateLimiterMemoryStoreConfig{Rate: rate.Limit(10), Burst: 30, ExpiresIn: 3 * time.Minute},
+		),
+		IdentifierExtractor: func(ctx echo.Context) (string, error) {
+			id := ctx.RealIP()
+			return id, nil
+		},
+		ErrorHandler: func(context echo.Context, err error) error {
+			return context.JSON(http.StatusForbidden, nil)
+		},
+		DenyHandler: func(context echo.Context, identifier string, err error) error {
+			return context.JSON(http.StatusTooManyRequests, nil)
+		},
 	}
 
 	group := e.Group("/v1/posts", middleware.EnsureAuthenticated(di))
@@ -23,6 +44,6 @@ func setupPostRoutes(e *echo.Echo, di *pkg.Di) {
 	group.PUT("/:id", postHandler.UpdatePost)
 	group.DELETE("/:id", postHandler.DeletePost)
 	group.GET("/user/:userId", postHandler.GetByUserID)
-	group.POST("/:id/like", postHandler.LikePost)
-	group.DELETE("/:id/like", postHandler.UnlikePost)
+	group.POST("/:id/like", postHandler.LikePost, echomiddleware.RateLimiterWithConfig(config))
+	group.DELETE("/:id/like", postHandler.UnlikePost, echomiddleware.RateLimiterWithConfig(config))
 }
